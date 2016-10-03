d46ceaf17f544218dc93483f5d5fe5bc238d52c2
timonsk <regetadm@gmail.com>
2016-10-03 15:10:24 +0300

    Clean up ObjectName

--- Infrastructure/Extensions/ObjectName.cs
+++ Infrastructure/Extensions/ObjectName.cs
@@ -7,34 +7,6 @@ namespace Infrastructure.Extensions
 {
     public static class ObjectName
     {
-        public static string GetPropertyName<TPropertySource>
-            (Expression<Func<TPropertySource, object>> expression)
-        {
-            var lambda = expression as LambdaExpression;
-            MemberExpression memberExpression;
-            if (lambda.Body is UnaryExpression)
-            {
-                var unaryExpression = lambda.Body as UnaryExpression;
-                memberExpression = unaryExpression.Operand as MemberExpression;
-            }
-            else
-            {
-                memberExpression = lambda.Body as MemberExpression;
-            }
-
-            Debug.Assert(memberExpression != null,
-                "Please provide a lambda expression like 'n => n.PropertyName'");
-
-            if (memberExpression != null)
-            {
-                var propertyInfo = memberExpression.Member as PropertyInfo;
-
-                return propertyInfo.Name;
-            }
-
-            return null;
-        }
-
         public static string GetPropertyName<T>(Expression<Func<T>> propertyLambda)
         {
             var me = propertyLambda.Body as MemberExpression;
